8dd9be4ce6a3889562eff07743531e454a901b2e
Deon Poncini <deonponcini@gmail.com>
2020-11-27 18:56:55 -0800

    Initial welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Jumbotron, Form} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = {};
+type AppState = {
+};
+
+class App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return(
+            <div className="bgimage">
+                <Jumbotron>
+                <Form>
+                <Form.Group controlId="seed">
+                    <Form.Control type="text" placeholder="Seed" />
+                </Form.Group>
+                <Form.Group controlId="run">
+                    <Form.Check inline type="radio" label="Any %"
+                        name="runtype" id="runtypeany" />
+                    <Form.Check inline type="radio" label="Dark Side"
+                        name="runtype" id="runtypedark" />
+                    <Form.Check inline type="radio" label="Darker Side"
+                        name="runtype" id="runtypedarker" />
+                    <Form.Check inline type="radio" label="All Moons"
+                        name="runtype" id="runtypeall" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Generate
+                </Button>
+                </Form>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
