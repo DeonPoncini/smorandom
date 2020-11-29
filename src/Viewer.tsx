852f2ea1c901d59cb983b85fc3a8c7f560286fc9
Deon Poncini <deonponcini@gmail.com>
2020-11-28 17:48:56 -0800

    Remove carousels, they are a distraction
    Build it with just simple buttons

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Carousel, Jumbotron} from 'react-bootstrap';
+import {Button, Container, Row, Col, Jumbotron} from 'react-bootstrap';
 import {Output} from './state';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Viewer.css';
@@ -8,6 +8,7 @@ type ViewerProps = {
     output: Output
 }
 type ViewerState = {
+    current_index: number;
 }
 
 class Viewer extends React.Component<ViewerProps, ViewerState> {
@@ -15,22 +16,67 @@ class Viewer extends React.Component<ViewerProps, ViewerState> {
     constructor(props: ViewerProps) {
         super(props);
         this.state = {
+            current_index: 0
         };
     }
 
+    previous() {
+        let index = this.state.current_index - 1;
+        this.setState({current_index: index});
+    }
+
+    next() {
+        let index = this.state.current_index + 1;
+        this.setState({current_index: index});
+    }
+
     render() {
+        // check if previous is disabled
+        let prev_disabled = false;
+        if (this.state.current_index === 0) {
+            prev_disabled = true;
+        }
+        // check if next is disabled
+        let next_disabled = false;
+        if (this.state.current_index === this.props.output.kingdoms.length-1) {
+            next_disabled = true;
+        }
+        let bgimage = {
+            backgroundImage: 'url("images/smohero.jpg")'
+        };
         return(
             <div className="slide">
-            <Carousel wrap={false}>
-                {this.props.output.kingdoms.map((kingdom) => (
-                    <Carousel.Item key={kingdom[0]} interval={undefined}>
-                    <div className="contents">
+            <div style={bgimage}>
+                <Container>
+                    <Row>
+                    <Col>
+                        <Button type="primary" disabled={prev_disabled}
+                            onClick={this.previous.bind(this)}>
+                            Previous
+                        </Button>
+                    </Col>
+                    <Col>
                         <Jumbotron>
-                           <h1>{kingdom[0]}</h1>
+                        <h1>
+                            {this.props.output.kingdoms[
+                                this.state.current_index][0]}
+                        </h1>
                         </Jumbotron>
+                    </Col>
+                    <Col>
+                        <Button type="primary" disabled={next_disabled}
+                            onClick={this.next.bind(this)}>
+                            Next
+                        </Button>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <div className="contents">
                         <div className="list">
                             <div>
-                               {kingdom[1].map((moon) => (
+                               {this.props.output.kingdoms[
+                                   this.state.current_index][1]
+                                       .map((moon) => (
                                    <div>
                                    <div className="entry">
                                        {moon[0]}
@@ -40,10 +86,10 @@ class Viewer extends React.Component<ViewerProps, ViewerState> {
                                ))}
                             </div>
                         </div>
-                    </div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
             </div>
         );
     }
